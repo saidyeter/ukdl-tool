53e427174614615f4d3397dfee815234236ffe1b
Said Yeter <kordiseps@gmail.com>
2025-12-16 22:09:30 +0300

    runWithoutProxy

--- index.js
+++ index.js
@@ -5,7 +5,79 @@ import { checkChange } from './pages/check-change.js';
 import { login } from './pages/login.js';
 import { proxies } from './proxies.js';
 
-(async () => {
+// SAIDA657054SM9IJ
+// EMRE657054MORGA
+// ADNAN57M9IJ054S
+
+const username = 'ADNAN57M9IJ054S';
+const refNumber = '1234567890';
+
+runWithoutProxy()
+
+async function runWithoutProxy() {
+  // Launch the browser and open a new blank page.
+  const browser = await puppeteer.launch({ headless: false });
+
+  try {
+    await run(browser);
+  } catch (error) {
+    if (error.message.includes('ERR_NAME_NOT_RESOLVED')) {
+      log('ERR_NAME_NOT_RESOLVED');
+    } else if (error.message.includes('ERR_CONNECTION_CLOSED')) {
+      log('ERR_CONNECTION_CLOSED');
+    } else if (error.message.includes('ERR_TUNNEL_CONNECTION_FAILED')) {
+      log('ERR_TUNNEL_CONNECTION_FAILED');
+    } else {
+      log('unexpected error:', error);
+    }
+  }
+  finally {
+    // await browser.close();
+  }
+  await new Promise(resolve => setTimeout(resolve, 1000));
+}
+
+async function run(browser, proxy) {
+
+  if (!BASEURL) {
+    throw new Error('BASEURL is not defined');
+  }
+  const args = [];
+  if (proxy) {
+    args.push('--proxy-server=' + proxy);
+  }
+  if (!browser) {
+    browser = await puppeteer.launch({ headless: false, args });
+  }
+  // Launch the browser and open a new blank page.
+  const page = await browser.newPage();
+
+  await page.goto(BASEURL);
+  console.log('successfull using proxy', proxy);
+
+  if (!await login(page, username, refNumber)) {
+    log('Login failed');
+    await browser.close();
+    return;
+  }
+  else {
+    log('Login success');
+  }
+  return
+
+  if (await checkChange(page)) {
+    log('There is change test centre');
+  }
+  else {
+    log('There is NO change test centre');
+  }
+
+  // await browser.close();
+}
+
+
+
+async function runWithProxy() {
   for (let i = 0; i < proxies.length; i++) {
     const proxy = proxies[i];
     log('trying proxy', proxy);
@@ -34,53 +106,5 @@ import { proxies } from './proxies.js';
     }
     await new Promise(resolve => setTimeout(resolve, 1000));
   }
-
-})()
-// run().catch(error => console.log('error2', error));
-
-
-
-// SAIDA657054SM9IJ
-// EMRE657054MORGA
-// ADNAN57M9IJ054S
-
-const username = 'ADNAN57M9IJ054S';
-const refNumber = '1234567890';
-
-
-export async function run(browser, proxy) {
-
-  if (!BASEURL) {
-    throw new Error('BASEURL is not defined');
-  }
-  const args = [];
-  if (proxy) {
-    args.push('--proxy-server=' + proxy);
-  }
-  if (!browser) {
-    browser = await puppeteer.launch({ headless: false, args });
-  }
-  // Launch the browser and open a new blank page.
-  const page = await browser.newPage();
-
-  await page.goto(BASEURL);
-  console.log('successfull using proxy', proxy);
-  return
-
-  if (!await login(page, username, refNumber)) {
-    log('Login failed');
-    await browser.close();
-    return;
-  }
-  else {
-    log('Login success');
-  }
-  if (await checkChange(page)) {
-    log('There is change test centre');
-  }
-  else {
-    log('There is NO change test centre');
-  }
-
-  // await browser.close();
 }
+
